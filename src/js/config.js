279f04e6fca2a04015699e101a2d5f1fe0ce0959
Lauri Jokipii <latec@iki.fi>
2017-04-19 15:33:53 +0300

    Tidy up a bit

--- src/js/config.js
+++ src/js/config.js
@@ -35,27 +35,27 @@ let user = {
 let domain_organization = {
   '@csc.fi': '00000' // debug/develop/demo (de3)
 
-  ,'@arcada.fi': '02535' //Arcada - Nylands svenska yrkeshögskola  biblioteket@arcada.fi #arcada-admins
-  ,'@centria.fi': '02536' //Centria-ammattikorkeakoulu  marjo.pekola@centria  #centria-admins
-  ,'@diak.fi': '02623' //Diakonia-ammattikorkeakoulu  julkaisutiedot@diak.fi  #diak-admins
-  ,'@haaga-helia.fi': '10056' //Haaga-Helia ammattikorkeakoulu  kirjasto.pasila@haaga-helia.fi  #haaga-helia-admins
-  ,'@humak.fi': '02631' //Humanistinen ammattikorkeakoulu  kirjasto@humak.fi #humak-admins
-  ,'@jamk.fi': '02504' //Jyväskylän ammattikorkeakoulu  helpdesk@jamk.fi  #jamk-admins
-  ,'@kamk.fi': '02473' //Kajaanin ammattikorkeakoulu  amkkirjasto@kamk.fi #kamk-admins
-  ,'@karelia.fi': '02469' //Karelia-ammattikorkeakoulu  julkaisut@karelia.fi  #karelia-admins
-  ,'@xamk.fi': '10118' //(mahd. kyamk.fi ja mamk.fi) //Kaakkois-Suomen ammattikorkeakoulu  julkaisut@xamk.fi #xamk-admins
-  ,'@lamk.fi': '02470' //Lahden ammattikorkeakoulu  pyydetty  #lamk-admins (odottaa jäseniä)
-  ,'@laurea.fi': '02629' //Laurea-ammattikorkeakoulu  julkaisut@laurea.fi #laurea-admins
-  ,'@metropolia.fi': '10065' //Metropolia ammattikorkeakoulu  annika.hayrynen@metropolia.fi #metropolia-admins
-  ,'@samk.fi': '02507' //Satakunnan ammattikorkeakoulu  lib.samk@samk.fi  #samk-admins
-  ,'@seamk.fi': '02472' //Seinäjoen ammattikorkeakoulu  julkaisutuki@seamk.fi #seamk-admins
-  ,'@tamk.fi': '02630' //Tampereen ammattikorkeakoulu  tiina.kenttala-koivumaki@tamk.fi  #tamk-admins
-  ,'@novia.fi': '10066' //Yrkeshögskolan Novia  johanna.glader@novia.fi #novia-admins
-  ,'@polamk.fi': '02557' //Poliisiammattikorkeakoulu  kirjasto@polamk.fi  #polamk-admins
+  ,'@arcada.fi': '02535' //Arcada - Nylands svenska yrkeshögskola #arcada-admins
+  ,'@centria.fi': '02536' //Centria-ammattikorkeakoulu  #centria-admins
+  ,'@diak.fi': '02623' //Diakonia-ammattikorkeakoulu  #diak-admins
+  ,'@haaga-helia.fi': '10056' //Haaga-Helia ammattikorkeakoulu  #haaga-helia-admins
+  ,'@humak.fi': '02631' //Humanistinen ammattikorkeakoulu  #humak-admins
+  ,'@jamk.fi': '02504' //Jyväskylän ammattikorkeakoulu  #jamk-admins
+  ,'@kamk.fi': '02473' //Kajaanin ammattikorkeakoulu  #kamk-admins
+  ,'@karelia.fi': '02469' //Karelia-ammattikorkeakoulu  #karelia-admins
+  ,'@xamk.fi': '10118' //(mahd. kyamk.fi ja mamk.fi) //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+  ,'@lamk.fi': '02470' //Lahden ammattikorkeakoulu  #lamk-admins (odottaa jäseniä)
+  ,'@laurea.fi': '02629' //Laurea-ammattikorkeakoulu  #laurea-admins
+  ,'@metropolia.fi': '10065' //Metropolia ammattikorkeakoulu  #metropolia-admins
+  ,'@samk.fi': '02507' //Satakunnan ammattikorkeakoulu  #samk-admins
+  ,'@seamk.fi': '02472' //Seinäjoen ammattikorkeakoulu  #seamk-admins
+  ,'@tamk.fi': '02630' //Tampereen ammattikorkeakoulu  #tamk-admins
+  ,'@novia.fi': '10066' //Yrkeshögskolan Novia  #novia-admins
+  ,'@polamk.fi': '02557' //Poliisiammattikorkeakoulu  #polamk-admins
 
   // tutkimusorganisaatio
-  ,'@fmi.fi': '4940015' //Ilmatieteen laitos  achim.drebs@fmi.fi  #fmi-admins
-  ,'@nls.fi': '4020217' //Maanmittauslaitos maanmittauslaitos.fi   MML.VIRTA@maanmittauslaitos.fi  #mml-admins
+  ,'@fmi.fi': '4940015' //Ilmatieteen laitos  #fmi-admins
+  ,'@nls.fi': '4020217' //Maanmittauslaitos maanmittauslaitos.fi   #mml-admins
 
   //,'@': '' //...
 };
