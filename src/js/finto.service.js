c9d56a13847e177c6e472df755b263a639a1e200
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-05-17 11:41:46 +0300

    Oops, typo. Must use HTTPS.

--- src/js/finto.service.js
+++ src/js/finto.service.js
@@ -2,7 +2,7 @@
 
 justusApp.service('FintoService', ['$http', function($http) {
   this.search = function(lang,input) {
-    var uri = "http://api.finto.fi/rest/v1/yso/search?type=skos%3AConcept&lang="+lang+"&query="+input+"*";
+    var uri = "https://api.finto.fi/rest/v1/yso/search?type=skos%3AConcept&lang="+lang+"&query="+input+"*";
     return $http.get(uri);
   }
 }]);
