30c7481972bab61a82e3d85fdf6d64249c37bd6e
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-05-30 11:34:05 +0300

    Clean out some not needed things.

--- src/config.js
+++ src/config.js
@@ -31,12 +31,12 @@ let user = {};
 if (demomode) {
   user = {
     //'shib-uid': 'jdem',
-    //'shib-mail': 'justus@csc.fi',
+    //'shib-mail': 'notvalid@csc.fi',
     //'shib-givenName': 'Justus',
     //'shib-sn': 'Demo',
     //'shib-group': '@csc.fi;https://tt.eduuni.fi/groups/justus#group-admins', // 
     'uid': 'jdem', // should map from shib-uid
-    'mail': 'justus@csc.fi', // should map from shib-mail
+    'mail': 'notvalid@csc.fi', // should map from shib-mail
     'name': 'Justus Demo', // should join shib-givenName and shib-sn
     'domain': '@csc.fi', // should get from shib-group where split(;) array item is '@...'
     'organization': {code:'10056',email:'notvalid@haaga-helia.fi'}, // should map from shib-group where split(;) array item is '@...'
@@ -46,36 +46,36 @@ if (demomode) {
 
 // mapping of organization domain to organization codes (type or codeset vary!)
 let domain_organization = {
-  '@csc.fi': {code:'00000',email:'justus@csc.fi'} // debug/develop/demo (de3)
+  '@csc.fi': {code:'00000',email:'notvalid@csc.fi'} // debug/develop/demo (de3)
 
-  ,'@arcada.fi': {code:'02535',email:'biblioteket@arcada.fi'} //Arcada - Nylands svenska yrkeshögskola  #arcada-admins
-  ,'@centria.fi': {code:'02536',email:'marjo.pekola@centria'} //Centria-ammattikorkeakoulu  #centria-admins
-  ,'@diak.fi': {code:'02623',email:'julkaisutiedot@diak.fi'} //Diakonia-ammattikorkeakoulu  #diak-admins
-  ,'@haaga-helia.fi': {code:'10056',email:'kirjasto.pasila@haaga-helia.fi'} //Haaga-Helia ammattikorkeakoulu  #haaga-helia-admins
-  ,'@humak.fi': {code:'02631',email:'kirjasto@humak.fi'} //Humanistinen ammattikorkeakoulu  #humak-admins
-  ,'@jamk.fi': {code:'02504',email:'helpdesk@jamk.fi'} //Jyväskylän ammattikorkeakoulu  #jamk-admins
-  ,'@kamk.fi': {code:'02473',email:'amkkirjasto@kamk.fi'} //Kajaanin ammattikorkeakoulu  #kamk-admins
-  ,'@karelia.fi': {code:'02469',email:'julkaisut@karelia.fi'} //Karelia-ammattikorkeakoulu  #karelia-admins
+  ,'@arcada.fi': {code:'02535',email:'notvalid@arcada.fi'} //Arcada - Nylands svenska yrkeshögskola  #arcada-admins
+  ,'@centria.fi': {code:'02536',email:'notvalid@centria'} //Centria-ammattikorkeakoulu  #centria-admins
+  ,'@diak.fi': {code:'02623',email:'notvalid@diak.fi'} //Diakonia-ammattikorkeakoulu  #diak-admins
+  ,'@haaga-helia.fi': {code:'10056',email:'notvalid@haaga-helia.fi'} //Haaga-Helia ammattikorkeakoulu  #haaga-helia-admins
+  ,'@humak.fi': {code:'02631',email:'notvalid@humak.fi'} //Humanistinen ammattikorkeakoulu  #humak-admins
+  ,'@jamk.fi': {code:'02504',email:'notvalid@jamk.fi'} //Jyväskylän ammattikorkeakoulu  #jamk-admins
+  ,'@kamk.fi': {code:'02473',email:'notvalid@kamk.fi'} //Kajaanin ammattikorkeakoulu  #kamk-admins
+  ,'@karelia.fi': {code:'02469',email:'notvalid@karelia.fi'} //Karelia-ammattikorkeakoulu  #karelia-admins
   // nb! xamk may have 3 domains (mahd. kyamk.fi ja mamk.fi)
-  ,'@xamk.fi': {code:'10118',email:'julkaisut@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
-  ,'@kyamk.fi': {code:'10118',email:'julkaisut@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
-  ,'@mamk.fi': {code:'10118',email:'julkaisut@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+  ,'@xamk.fi': {code:'10118',email:'notvalid@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+  ,'@kyamk.fi': {code:'10118',email:'notvalid@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+  ,'@mamk.fi': {code:'10118',email:'notvalid@xamk.fi'} //Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
 
-  ,'@lamk.fi': {code:'02470',email:'julkaisut@lamk.fi'} //Lahden ammattikorkeakoulu  #lamk-admins
-  ,'@laurea.fi': {code:'02629',email:'julkaisut@laurea.fi'} //Laurea-ammattikorkeakoulu  #laurea-admins
-  ,'@metropolia.fi': {code:'10065',email:'annika.hayrynen@metropolia.fi'} //Metropolia ammattikorkeakoulu  #metropolia-admins
-  ,'@samk.fi': {code:'02507',email:'lib.samk@samk.fi'} //Satakunnan ammattikorkeakoulu  #samk-admins
-  ,'@seamk.fi': {code:'02472',email:'julkaisutuki@seamk.fi'} //Seinäjoen ammattikorkeakoulu  #seamk-admins
-  ,'@tamk.fi': {code:'02630',email:'tiina.kenttala-koivumaki@tamk.fi'} //Tampereen ammattikorkeakoulu  #tamk-admins
-  ,'@novia.fi': {code:'10066',email:'johanna.glader@novia.fi'} //Yrkeshögskolan Novia  #novia-admins
-  ,'@polamk.fi': {code:'02557',email:'kirjasto@polamk.fi'} //Poliisiammattikorkeakoulu  #polamk-admins
+  ,'@lamk.fi': {code:'02470',email:'notvalid@lamk.fi'} //Lahden ammattikorkeakoulu  #lamk-admins
+  ,'@laurea.fi': {code:'02629',email:'notvalid@laurea.fi'} //Laurea-ammattikorkeakoulu  #laurea-admins
+  ,'@metropolia.fi': {code:'10065',email:'notvalid@metropolia.fi'} //Metropolia ammattikorkeakoulu  #metropolia-admins
+  ,'@samk.fi': {code:'02507',email:'notvalid@samk.fi'} //Satakunnan ammattikorkeakoulu  #samk-admins
+  ,'@seamk.fi': {code:'02472',email:'notvalid@seamk.fi'} //Seinäjoen ammattikorkeakoulu  #seamk-admins
+  ,'@tamk.fi': {code:'02630',email:'notvalid@tamk.fi'} //Tampereen ammattikorkeakoulu  #tamk-admins
+  ,'@novia.fi': {code:'10066',email:'notvalid@novia.fi'} //Yrkeshögskolan Novia  #novia-admins
+  ,'@polamk.fi': {code:'02557',email:'notvalid@polamk.fi'} //Poliisiammattikorkeakoulu  #polamk-admins
 
   // tutkimusorganisaatio
-  ,'@fmi.fi': {code:'4940015',email:'achim.drebs@fmi.fi'} //Ilmatieteen laitos  #fmi-admins
+  ,'@fmi.fi': {code:'4940015',email:'notvalid@fmi.fi'} //Ilmatieteen laitos  #fmi-admins
   
   // nb! mml has 2 domains
-  ,'@nls.fi': {code:'4020217',email:'MML.VIRTA@maanmittauslaitos.fi'} //Maanmittauslaitos  #mml-admins
-  ,'@maanmittauslaitos.fi': {code:'4020217',email:'MML.VIRTA@maanmittauslaitos.fi'} //Maanmittauslaitos  #mml-admins
+  ,'@nls.fi': {code:'4020217',email:'notvalid@maanmittauslaitos.fi'} //Maanmittauslaitos  #mml-admins
+  ,'@maanmittauslaitos.fi': {code:'4020217',email:'notvalid@maanmittauslaitos.fi'} //Maanmittauslaitos  #mml-admins
 
   //,'@': '' //...
 };
