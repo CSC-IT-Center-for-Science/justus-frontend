b06d693221893a1262557d05afc8b907af59669a
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-09-29 11:54:44 +0300

    Remove generated config from version control

--- src/app/js/environment_config.js
+++
@@ -1,5 +0,0 @@
-angular.module("appConfig", [])
-.constant("SITE_URL", "http://10.10.10.10")
-.constant("API_BASE_URL", "http://10.10.10.10/api/")
-.constant("AUTH_URL", "http://10.10.10.10/sec/api/auth.php")
-.constant("DEMO_ENABLED", true);
